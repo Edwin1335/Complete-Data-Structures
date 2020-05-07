2f754855cf1ccc8c888538be31a6e06173efc57c
Edwin Campuzano <campuzano.edwin1995@gmail.com>
2020-05-07 18:26:39 -0500

    Deleted system(pause)

--- ArrayQueues/Source.cpp
+++ ArrayQueues/Source.cpp
@@ -2,20 +2,18 @@
 
 int main()
 {
-	Queue x(5);
-	x.enQueue(100);
-	x.enQueue(200);
-	x.enQueue(400);
-	x.enQueue(500);
-	x.enQueue(600);
+    Queue x(5);
+    x.enQueue(100);
+    x.enQueue(200);
+    x.enQueue(400);
+    x.enQueue(500);
+    x.enQueue(600);
     x.deQueue();
-	x.print();
-	if (x.search(234))
-		cout << "Found";
-	else
-		cout << "Not found" << endl;
+    x.print();
+    if (x.search(234))
+        cout << "Found";
+    else
+        cout << "Not found" << endl;
 
-	system("pause");
-    system("CLS");
-	return 0;
+    return 0;
 }
